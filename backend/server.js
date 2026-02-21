b8cff8dce3267ea4936f619373d78ed380dc6b7e
Arvee <arvee.odon@cvsu.edu.ph>
2026-02-21 11:04:54 +0800

    Modify: make ready to deploy in vercel

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true, // allow client to send cookies
-    }),
-  );
-}
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true, // allow client to send cookies
+  }),
+);
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser()); // parse cookie request body
@@ -35,14 +33,6 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api/v1"
-      : "/api/v1",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   withCredentials: true, // this is just to send the cookies with the request
 });
